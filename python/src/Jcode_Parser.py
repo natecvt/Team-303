caeb743acc1bf813839355ff1dbef6e32373caad
JacobK21 <jacobk21@vt.edu>
2025-10-29 18:19:21 -0400

    Added more functionality to code.

--- python/src/Jcode_Parser.py
+++ python/src/Jcode_Parser.py
@@ -1,25 +1,26 @@
 import json
+array = ["PRINT_COMPLETE", "ERROR"]
+
 def print_event(dct):
     if 'event_type' in dct:
         return str(dct['event_type'])
     return dct
 
-with open('/home/kenne/Team-303/python/src/msg.json', 'r') as file:
-    data = json.load(file, object_hook=print_event)
+def printer_status(current_status):
+    if current_status == "PRINT_COMPLETE":
+        print("The 3D print job has completed successfully.")
+    elif current_status == "ERROR":
+        print("An error has occurred during the 3D printing process.") 
+    else:
+        print("Unknown event type.") 
+    return
+    
 
 def main():
-    print(data)
+    with open('/home/kenne/Team-303/python/src/msg.json', 'r') as file:
+        status = json.load(file, object_hook=print_event)
 
-    pass
-
-
-
-
-
-
-
-
-
+    printer_status(status)
 
 if __name__ == "__main__":
 
