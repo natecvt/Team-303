f817ec33f585ae09589beed7f1fd270978cbedad
JacobK21 <jacobk21@vt.edu>
2025-11-10 15:07:10 -0500

    Added functions from gcode_gen

--- python/src/Gcode_JLK.py
+++ python/src/Gcode_JLK.py
@@ -1,16 +1,7 @@
+from gcode_gen import generate_code, gcode_template, mcode_template
 
 
-gcode_template = {
-   0 : ["G0 Xx Yy Zz\n", (1, 2, 3, 4)],
-   1 : ["G1 Xx Yy Zz Ff\n", (1, 2, 3, 4)],
-   2 : ["G2 Xx Yy Zz Rr\n", (2, 3)],
-   3 : ["G3 Xx Yy Zz Rr\n", (2, 3)],
-   4 : ["G4 Pp\n", (1)],
-   5 : ["G54 Xx Yy Zz\n", (1, 2, 3, 4)],
-   6 : ["M00\n", (0)],
-}
-
-printer_loc = [
+printer_loc = {
     0 : {"x": 0.0,   "y": 0.0},   
     1 : {"x": 1.1,   "y": 0.1},  
     2 : {"x": 100.0, "y": 0.0},  
@@ -24,30 +15,8 @@ printer_loc = [
     10 : {"x": 50.0,  "y": 100.0},
     11 : {"x": 50.0,  "y": 50.0},  
     12 : {"x": 75.0,  "y": 50.0},
-]
-
-
-def generate_gcode(command_args, command_type):
-    if command_type not in gcode_template:
-        raise ValueError("Invalid command type")
-    
-    if not isinstance(command_args, dict):
-        raise TypeError("command_args must be a dictionary")
-
-    template = str(gcode_template[command_type][0])
-    possible_nargs = tuple(gcode_template[command_type][1])
-    nargs = len(command_args)
-
-    if possible_nargs.count(nargs) == 0:
-        raise ValueError("Invalid number of arguments for command type")
-    
-    else:
-        gcode = template
-        for i in command_args:
-            gcode = gcode.replace(command_args[i], f"{i:.1f}")
-
-        return gcode
-
+}
+ 
 
 def printers(printer_number):
     if not isinstance(printer_number, int):
